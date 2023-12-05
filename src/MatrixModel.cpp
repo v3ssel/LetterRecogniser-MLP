c2fa720bfaf7f47ac479172cdad64669ed93acc4
Gabriela Lupita <gabriela@pr-p3.kzn.21-school.ru>
2023-12-05 21:35:58 +0300

    thinking

---
+++ src/MatrixModel.cpp
@@ -0,0 +1,20 @@
+#include "MatrixModel.h"
+
+bool MatrixModel::setWeights() {
+
+    return true;
+}
+
+std::vector<std::vector<double>> MatrixModel::feedForward(std::vector<std::vector<double>> &input_layer) {
+
+    return std::vector<std::vector<double>>();
+}
+
+void MatrixModel::backPropagation(std::vector<std::vector<double>> &output_layer) {
+    
+}
+
+bool MatrixModel::updateWeights() {
+
+    return true;
+}
---
+++ src/MultilayerPerceptron.cpp
@@ -0,0 +1,32 @@
+#include "MultilayerPerceptron.h"
+
+std::vector<std::vector<double>> s21::MultilayerPerceptron::inputData() {
+
+    return std::vector<std::vector<double>>();
+}
+
+std::vector<std::vector<double>> s21::MultilayerPerceptron::importWeights() {
+    model.setWeights();
+    return std::vector<std::vector<double>>();
+}
+
+std::vector<std::vector<double>> s21::MultilayerPerceptron::learning() {
+    auto output_layer = model.feedForward(input_layer);
+    model.backPropagation(output_layer);
+    return std::vector<std::vector<double>>();
+}
+
+int s21::MultilayerPerceptron::getHiddenLayers() {
+    return hidden_layers;
+}
+
+void s21::MultilayerPerceptron::setHiddenLayers(int n) {
+    if (n > 1 && n < 6)
+        hidden_layers = n;
+}
+
+char s21::MultilayerPerceptron::prediction() {
+    auto output_layer = model.feedForward(input_layer);
+
+    return 0;
+}
