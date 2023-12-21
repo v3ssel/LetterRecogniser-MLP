1c6b9e37aa386e4bfb435ccd4e6988e72347cf07
v3ssel <v3ssels@pm.me>
2023-12-21 18:17:13 +0600

    matrix model tests

---
+++ src/tests/MatrixModelTests.cpp
@@ -0,0 +1,121 @@
+#include <gtest/gtest.h>
+#include "../core/matrix/MatrixModel.h"
+
+
+TEST(MatrixModel, LearningRate) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+    EXPECT_EQ(model->getLearningRate(), 0.1);
+    model->setLearningRate(0.15);
+    EXPECT_EQ(model->getLearningRate(), 0.15);
+}
+
+TEST(MatrixModel, GetLayerSizes) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+
+    auto layer_sizes = model->getLayersSize();
+
+    EXPECT_EQ(layer_sizes.size(), 4);
+    EXPECT_EQ(layer_sizes[0], 10);
+    EXPECT_EQ(layer_sizes[1], 5);
+    EXPECT_EQ(layer_sizes[2], 5);
+    EXPECT_EQ(layer_sizes[3], 3);
+}
+
+TEST(MatrixModel, GetWeights) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+    model->randomFill();
+
+    auto weights = model->getWeights();
+
+    EXPECT_EQ(weights.size(), 90);
+    EXPECT_TRUE(std::any_of(weights.begin(), weights.end(), [](double v) { return v != 0; }));
+}
+
+TEST(MatrixModel, GetBiases) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+    model->randomFill();
+
+    auto biases = model->getBiases();
+
+    EXPECT_EQ(biases.size(), 13);
+    EXPECT_TRUE(std::any_of(biases.begin(), biases.end(), [](double v) { return v != 0; }));
+}
+
+TEST(MatrixModel, RandomFill) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+    auto weights = model->getWeights();
+    auto biases = model->getBiases();
+
+    EXPECT_TRUE(std::all_of(weights.begin(), weights.end(), [](double v) { return v == 0; }));
+    EXPECT_TRUE(std::all_of(biases.begin(), biases.end(), [](double v) { return v == 0; }));
+
+    model->randomFill();
+    weights = model->getWeights();
+    biases = model->getBiases();
+
+    EXPECT_TRUE(std::any_of(weights.begin(), weights.end(), [](double v) { return v != 0; }));
+    EXPECT_TRUE(std::any_of(biases.begin(), biases.end(), [](double v) { return v != 0; }));
+}
+
+TEST(MatrixModel, SetWeights) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+    auto weights = model->getWeights();
+
+    EXPECT_EQ(weights.size(), 90);
+    EXPECT_TRUE(std::all_of(weights.begin(), weights.end(), [](double v) { return v == 0; }));
+
+    std::vector<double> new_weights;
+    for (int i = 0; i < weights.size(); i++) new_weights.push_back(i);
+
+    model->setWeights(new_weights);
+    weights = model->getWeights();
+
+    EXPECT_TRUE(new_weights == weights);
+}
+
+TEST(MatrixModel, SetBadWeights) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+
+    EXPECT_THROW(model->setWeights({1, 2, 3}), std::out_of_range);
+}
+
+TEST(MatrixModel, SetBiases) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+    auto biases = model->getBiases();
+    
+    EXPECT_EQ(biases.size(), 13);
+    EXPECT_TRUE(std::all_of(biases.begin(), biases.end(), [](double v) { return v == 0; }));
+
+    std::vector<double> new_biases;
+    for (int i = 0; i < biases.size(); i++) new_biases.push_back(i);
+
+    model->setBiases(new_biases);
+    biases = model->getBiases();
+
+    EXPECT_TRUE(new_biases == biases);
+}
+
+TEST(MatrixModel, SetBadBiases) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+
+    EXPECT_THROW(model->setBiases({1, 2, 3}), std::out_of_range);
+}
+
+TEST(MatrixModel, DefaultModel) {
+    std::unique_ptr<s21::MLPModel> model = std::make_unique<s21::MatrixModel>(10, 3, 2, 5, 0.1);
+
+    auto sizes = model->getLayersSize();
+    EXPECT_EQ(sizes.size(), 4);
+    EXPECT_EQ(sizes[0], 10);
+    EXPECT_EQ(sizes[1], 5);
+    EXPECT_EQ(sizes[2], 5);
+    EXPECT_EQ(sizes[3], 3);
+    
+    auto weights = model->getWeights();
+    EXPECT_TRUE(std::all_of(weights.begin(), weights.end(), [](double v) { return v == 0; }));
+    
+    auto biases = model->getBiases();
+    EXPECT_TRUE(std::all_of(biases.begin(), biases.end(), [](double v) { return v == 0; }));
+
+    EXPECT_EQ(model->getLearningRate(), 0.1);
+}
